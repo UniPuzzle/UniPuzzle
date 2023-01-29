3d6b99d5d317ddb36671d190f592852833a01c23
Vitaliy <chichygav@gmail.com>
2023-01-29 18:11:03 +0200

    Update drop.js

--- js/drop.js
+++ js/drop.js
@@ -1,4 +1,4 @@
-// Полифилл для метода forEach для NodeList
+// Поліфіл для методу forEach для NodeList
 if (window.NodeList && !NodeList.prototype.forEach) {
   NodeList.prototype.forEach = function (callback, thisArg) {
     thisArg = thisArg || window;
@@ -18,14 +18,14 @@ document.querySelectorAll('.dropdown').forEach(function (dropDownWrapper) {
     '.dropdown__input-hidden',
   );
 
-  // Клик по кнопке. Открыть/Закрыть select
+  // Клік по кнопці. Відкрити/Закрити select
   dropDownBtn.addEventListener('click', function (e) {
     dropDownList.classList.toggle('dropdown__list--visible');
     dropDownBtn.classList.toggle('dropdown__button-up');
     this.classList.add('dropdown__button--active');
   });
 
-  // Выбор элемента списка. Запомнить выбранное значение. Закрыть дропдаун
+  //Вибір елемента списку. Запам'ятати вибране значення. Закрити дропдаун
   dropDownListItems.forEach(function (listItem) {
     listItem.addEventListener('click', function (e) {
       e.stopPropagation();
@@ -37,7 +37,7 @@ document.querySelectorAll('.dropdown').forEach(function (dropDownWrapper) {
     });
   });
 
-  // Клик снаружи дропдауна. Закрыть дропдаун
+  //Клік зовні дропдауну. Закрити дропдаун
   document.addEventListener('click', function (e) {
     if (e.target !== dropDownBtn) {
       dropDownBtn.classList.remove('dropdown__button--active');
@@ -46,7 +46,7 @@ document.querySelectorAll('.dropdown').forEach(function (dropDownWrapper) {
     }
   });
 
-  // Нажатие на Tab или Escape. Закрыть дропдаун
+  // Натискання на Tab або Escape. Закрити дропдаун
   document.addEventListener('keydown', function (e) {
     if (e.key === 'Tab' || e.key === 'Escape') {
       dropDownBtn.classList.remove('dropdown__button--active');
